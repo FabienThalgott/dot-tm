6b505f8d06c2247ffab413cbbe1f6d3b105ce3f7
FabienThalgott <fabien.thalgott@gmail.com>
2018-03-06 14:19:09 +0100

    bug fix + input validation for the php file

--- generateGraph.php
+++ generateGraph.php
@@ -2,17 +2,20 @@
 error_reporting(E_ALL); ini_set('display_errors', 1);
 
 $filename = $_GET['file'];
-file_put_contents('graphs/'.$filename, file_get_contents('php://input'));
-shell_exec('python dot-tm.py graphs/'.$filename);
+if(preg_match('/^[a-z0-9-]+$/', $filename)) {
+    file_put_contents('graphs/'.$filename, file_get_contents('php://input'));
+    shell_exec('python dot-tm.py graphs/'.$filename);
 
-if($_GET['style']=='static'){
-	$imagedata = file_get_contents('graphs/'.$filename.'.png');
-    $base64 = base64_encode($imagedata);
-	echo $base64;
+    if($_GET['style']=='static'){
+            $imagedata = file_get_contents('graphs/'.$filename.'.png');
+     $base64 = base64_encode($imagedata);
+            echo $base64;
+    }
+
+    if($_GET['style']=='interactive'){
+            echo file_get_contents('graphs/'.$filename.'.dot');
+    }
+} else {
+echo "Nah, that won't work";
 }
-
-if($_GET['style']=='interactive'){
-	echo file_get_contents('graphs/'.$filename.'.dot');
-}
-
 ?>
